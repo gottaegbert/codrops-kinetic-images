a22170e57a670fe123628b7fcc09b9760938e69f
Manoela Ilic <crnacura@gmail.com>
2025-07-07 08:38:20 +0100

    New images

--- src/data/images.js
+++ src/data/images.js
@@ -1,43 +1,17 @@
 const images = [
-    {
-        url: 'https://cdn.cosmos.so/32873d53-db8f-454b-a609-a5f2a8a4b3df?format=jpeg',
-    },
-    {
-        url: 'https://cdn.cosmos.so/734df392-f98c-4223-88c5-79ccb97acc93?format=jpeg',
-    },
-    {
-        url: 'https://cdn.cosmos.so/a040b28a-7802-4dd6-9d5d-34c22d6d8413?format=jpeg',
-    },
-    {
-        url: 'https://cdn.cosmos.so/d58cea1b-ee69-4d24-b154-01a9043dd847?format=jpeg',
-    },
-    {
-        url: 'https://cdn.midjourney.com/2a426bf8-d0b8-425a-a3ed-3151406a3277/0_0.png',
-    },
-    {
-        url: 'https://cdn.cosmos.so/93ed9b37-5035-4e61-b730-c88bd9d13df1?format=jpeg',
-    },
-    {
-        url: 'https://cdn.cosmos.so/9b1db94b-cd0a-4bab-9dd2-5a96af43964e.?format=jpeg',
-    },
-    {
-        url: 'https://cdn.midjourney.com/4163c5b8-d4cb-428b-9d38-1b414a9a5cba/0_0.png',
-    },
-    {
-        url: 'https://cdn.cosmos.so/634a729a-3da7-4cff-b47a-551a72feedc7?format=jpeg',
-    },
-    {
-        url: 'https://cdn.midjourney.com/916614b6-4921-4bbb-84c0-a935f29eae7a/0_0.png',
-    },
-    {
-        url: 'https://cdn.cosmos.so/2756cb2d-420f-4096-8d7f-dd3ea55e3eb4?format=jpeg',
-    },
-    {
-        url: 'https://cdn.midjourney.com/9ad2f9da-2bbc-4f45-b2f0-7e3ecfd439a6/0_3.png',
-    },
-    {
-        url: 'https://cdn.cosmos.so/b1df4267-7dfb-4ee2-937b-b6fbc157be56.?format=jpeg',
-    },
+    { url: '/images/img1.webp' },
+    { url: '/images/img2.webp' },
+    { url: '/images/img3.webp' },
+    { url: '/images/img4.webp' },
+    { url: '/images/img5.webp' },
+    { url: '/images/img6.webp' },
+    { url: '/images/img7.webp' },
+    { url: '/images/img8.webp' },
+    { url: '/images/img9.webp' },
+    { url: '/images/img10.webp' },
+    { url: '/images/img11.webp' },
+    { url: '/images/img12.webp' },
+    { url: '/images/img13.webp' },
 ];
 
 export default images;

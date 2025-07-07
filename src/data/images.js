2d2b4e8e45d5d34553a364f03e1bdf121182af2e
DGFX <dgfxpro@gmail.com>
2025-07-07 10:35:13 +0200

    go back

--- src/data/images.js
+++ src/data/images.js
@@ -1,31 +1,17 @@
-import img1 from '@/assets/images/img1.webp';
-import img2 from '@/assets/images/img2.webp';
-import img3 from '@/assets/images/img3.webp';
-import img4 from '@/assets/images/img4.webp';
-import img5 from '@/assets/images/img5.webp';
-import img6 from '@/assets/images/img6.webp';
-import img7 from '@/assets/images/img7.webp';
-import img8 from '@/assets/images/img8.webp';
-import img9 from '@/assets/images/img9.webp';
-import img10 from '@/assets/images/img10.webp';
-import img11 from '@/assets/images/img11.webp';
-import img12 from '@/assets/images/img12.webp';
-import img13 from '@/assets/images/img13.webp';
-
 const images = [
-    { url: img1.src },
-    { url: img2.src },
-    { url: img3.src },
-    { url: img4.src },
-    { url: img5.src },
-    { url: img6.src },
-    { url: img7.src },
-    { url: img8.src },
-    { url: img9.src },
-    { url: img10.src },
-    { url: img11.src },
-    { url: img12.src },
-    { url: img13.src },
+    { url: '/images/img1.webp' },
+    { url: '/images/img2.webp' },
+    { url: '/images/img3.webp' },
+    { url: '/images/img4.webp' },
+    { url: '/images/img5.webp' },
+    { url: '/images/img6.webp' },
+    { url: '/images/img7.webp' },
+    { url: '/images/img8.webp' },
+    { url: '/images/img9.webp' },
+    { url: '/images/img10.webp' },
+    { url: '/images/img11.webp' },
+    { url: '/images/img12.webp' },
+    { url: '/images/img13.webp' },
 ];
 
 export default images;

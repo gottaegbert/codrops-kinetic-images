eb4764b427bb87e63c39da6663af1fce7407a888
胡思宇 <62612263+gottaegbert@users.noreply.github.com>
2025-08-28 18:17:41 +0200

    test

--- frontend/.next/static/chunks/frontend_src_app_layout_jsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/frontend_src_eb091d57._.css",
-  "static/chunks/frontend_src_webgl_Scene_jsx_b009430d._.js",
-  "static/chunks/_98d106af._.js"
-],
-    source: "dynamic"
-});
